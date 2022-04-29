09674e0ecc5b6dafedafaa70a6330434ff0297e9
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-04-28 22:20:50 -0500

    Add files via upload

---
+++ O(logn).js
@@ -0,0 +1,22 @@
+// Level 0: 2^0 = 1;
+// Level 1: 2^1 = 2;
+// Level 2: 2^2 = 4;
+// Level 3: 2^3 = 8;
+
+// # of nodes = 2^h - 1; Example 2 to the power of 2 of 3 is 8 - 1, equals a total of 7 nodes. 
+// O(log N) = similar of a phone book, divide and conquer look at whe the book is or pages. 
+// O(n) you need to check every element. O(log N) you don' need to check every elements
+// log nodes = steps
+
+// log 100 = 2
+// 10^2 = 100;
+
+// Binary Search Tree
+// Lookup O(log N)
+// Insert O(log N)
+// Delete O(log N)
+
+// IS 37 < 101, yes then go to the left 
+// Is 37 < 33, no go tho the right 
+// Don't have to iterate linear fashion
+
